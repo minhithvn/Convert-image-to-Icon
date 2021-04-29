e2b6ec30a5ec7a2b650a35a6c0cf0ad590a72f62
minhithvn <34228783+minhithvn@users.noreply.github.com>
2021-04-29 16:00:28 +0700

    Initial commit

---
+++ convertico/convertico/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Chọn đường dẫn ảnh"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(598, 37)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(204, 38)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Chọn ảnh và chuyển đổi"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(205, 37)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(370, 38)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Blue
+        Me.Label2.Location = New System.Drawing.Point(202, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 17)
+        Me.Label2.TabIndex = 6
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(886, 134)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Chuyển đổi định sang JPG,PNG sang ICO"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ convertico/convertico/Form1.vb
@@ -0,0 +1,58 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        'Dim open As New OpenFileDialog
+        'open.Filter = "Chọn ảnh bạn cần thêm (*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif"
+        'If open.ShowDialog = DialogResult.OK Then
+        '    PictureBox1.Image = Image.FromFile(open.FileName)
+        'End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim oOpenFileDialog As OpenFileDialog
+            Dim oSaveFileDialog As SaveFileDialog
+            Dim oBitmap As Bitmap
+            Dim HIcon As IntPtr
+            Dim newIcon As Icon
+        Dim oFileStream As IO.FileStream
+
+
+        oOpenFileDialog = New OpenFileDialog
+            oOpenFileDialog.Filter = "Chọn ảnh bạn cần thêm (*.jpg;*.png;*.gif)|*.jpg;*.png;*.gif"
+            If oOpenFileDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+                TextBox1.Text = oOpenFileDialog.FileName
+
+                ' Create a Bitmap object from an image file.
+                oBitmap = New Bitmap(oOpenFileDialog.FileName)
+
+                ' Get an Hicon for myBitmap.
+                HIcon = oBitmap.GetHicon()
+
+                ' Create a new icon from the handle.
+                newIcon = System.Drawing.Icon.FromHandle(HIcon)
+
+                ' Set the form Icon attribute to the new icon.
+                Me.Icon = newIcon
+            Try
+                'Save the bitmap to new file
+                oSaveFileDialog = New SaveFileDialog
+                oSaveFileDialog.Filter = "Định dạng icon (*.ico)|*.ico"
+                'oSaveFileDialog.AddExtension = True
+                If oSaveFileDialog.ShowDialog = Windows.Forms.DialogResult.OK Then
+                    oFileStream = New IO.FileStream(oSaveFileDialog.FileName, IO.FileMode.CreateNew)
+                    newIcon.Save(oFileStream)
+                    oFileStream.Close()
+                    'MsgBox("Đã lưu tệp ico thành công")
+                    Label2.Text = "Bạn đã lưu thành công định dạng ICO"
+
+                End If
+            Catch ex As Exception
+                MessageBox.Show("Có lỗi xảy ra với đường dẫn hoặc trùng tên ảnh, xin kiểm tra lại" &
+    vbNewLine & "Dưới đây là mã lỗi: " & vbNewLine & String.Format("Error: {0}", ex.Message))
+            End Try
+        End If
+
+    End Sub
+
+End Class
+
